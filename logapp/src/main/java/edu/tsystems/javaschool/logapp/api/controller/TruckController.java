86bdd1da97b4ed0b5e5e4ed544f408e1004ceea3
artesumen <61182711+artesumen@users.noreply.github.com>
2020-03-09 19:18:52 +0300

    Save() method for drivers is working with relationships to truck table. Exceptions classes added. Delete and Edit methods for Truck and Driver added.

--- logapp/src/main/java/edu/tsystems/javaschool/logapp/api/controller/TruckController.java
+++ logapp/src/main/java/edu/tsystems/javaschool/logapp/api/controller/TruckController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -68,6 +69,38 @@ public class TruckController {
 
         return "trucks/allTrucks";
     }
+
+    @RequestMapping(value = "/removeTruck/{id}",method = RequestMethod.GET)
+    public String removeTruck(@PathVariable("id") int id) {
+        truckService.removeTruck(id);
+        return "redirect:/allTrucks";
+    }
+
+    @RequestMapping(value="/editTruck/{id}",method = RequestMethod.GET)
+    public ModelAndView editShowForm (@PathVariable("id") int id, Model model){
+        ModelAndView mav = new ModelAndView("trucks/editTruck");
+        mav.addObject("truckToEdit",truckService.getTruckById(id));
+        mav.addObject("enumCondition", Truck.Condition.values());
+
+        return mav;
+    }
+
+    @RequestMapping(value = "/editTruck/{id}", method = RequestMethod.POST)
+    public String submitEdit(@ModelAttribute("truckToEdit") TruckDTO truck,
+                         ModelMap model) throws IOException {
+
+        model.addAttribute("id",truck);
+        model.addAttribute("regNumber", truck);
+        model.addAttribute("driverWorkingHours", truck);
+        model.addAttribute("capacityTons", truck);
+        model.addAttribute("condition", truck);
+        model.addAttribute("currentCityId", truck);
+        truckService.updateTruck(truck);
+
+        return "trucks/editTruck";
+    }
+
+
 
 
 
